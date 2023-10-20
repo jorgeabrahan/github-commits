cf2ccaddde86989297900431f395d3c0e47b85af
jorgeabrahan <jorge24abrahan@gmail.com>
2023-10-20 15:08:06 -0600

    Create: endpoint to fetch commits by author

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -13,5 +13,10 @@ export class AppController {
   @Get('commits')
   getCommits() {
     return this.appService.getCommits();
+  }
+
+  @Get('commits/author')
+  getCommitsByAuthor(@Query('author') author: string) {
+    return this.appService.getCommitsByAuthor(author);
   }
 }
