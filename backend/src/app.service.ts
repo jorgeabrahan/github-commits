1ef22b5eb9b7b1284f11c82770f3e32caeaf1c1e
jorgeabrahan <jorge24abrahan@gmail.com>
2023-10-20 15:02:03 -0600

    Create: method to fetch all commits from current repo

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
+  private readonly owner = 'jorgeabrahan';
+  private readonly repo = 'github-commits';
+  private readonly baseUrl = `https://api.github.com/repos/${this.owner}/${this.repo}`;
+
   getWelcome(): string {
     return 'Welcome to the GitHub commits NestJS application';
+  }
+
+  async getCommits() {
+    const { data } = await axios.get(`${this.baseUrl}/commits`);
+    return data;
   }
 }
