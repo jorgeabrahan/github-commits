b312f3bc960cb268f26997e67e78560c38e4a9e4
jorgeabrahan <jorge24abrahan@gmail.com>
2023-10-21 01:54:09 -0600

    Update: add tailwind styles and show the time offset of each commit

--- frontend/src/components/commits-c/Commit.tsx
+++ frontend/src/components/commits-c/Commit.tsx
@@ -1,19 +1,19 @@
+import { calcTimeOffset } from "../../helpers";
 import type { CommitI } from "../../interfaces";
 
 export const Commit = ({ commit }: { commit: CommitI }) => {
-  const [date, time] = commit?.commit?.committer?.date?.replace('Z', '')?.split('T')
   return (
-    <li>
-      <p>{commit?.commit?.message}</p>
-      <div>
-        <a href={commit?.committer?.avatar_url}>
-          <img src={commit?.committer?.avatar_url} alt={commit?.committer?.login} />
-        </a>
-        <a href={commit?.committer?.url}>{commit?.committer?.login}</a>
-        <span>{date}</span>
-        <br />
-        <span>{time}</span>
-      </div>
+    <li className="px-4 py-3 border-b border-solid border-slate-700 hover:bg-black/20">
+      <article>
+        <a href={commit?.html_url} target="_blank" className="font-medium mb-1">{commit?.commit?.message}</a>
+        <div className="flex gap-2">
+          <a href={commit?.committer?.avatar_url} target="_blank">
+            <img className="w-6 rounded-full aspect-square" src={commit?.committer?.avatar_url} alt={commit?.committer?.login} />
+          </a>
+          <a className="text-sm font-medium" href={commit?.committer?.url}>{commit?.committer?.login}</a>
+          <p className="text-sm text-slate-400">{calcTimeOffset(new Date(commit?.commit?.committer?.date))}</p>
+        </div>
+      </article>
     </li>
   );
 }
