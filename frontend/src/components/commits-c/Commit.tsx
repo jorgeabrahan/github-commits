9cf21facac6877d5e542aacbeda664dd0fe63fbe
jorgeabrahan <jorge24abrahan@gmail.com>
2023-10-21 13:37:32 -0600

    Update: backdrop blur of each commit

--- frontend/src/components/commits-c/Commit.tsx
+++ frontend/src/components/commits-c/Commit.tsx
@@ -3,7 +3,7 @@ import { CommitButtons, CommitInfo } from './commit-c';
 
 export const Commit = ({ commit }: { commit: CommitI }) => {
   return (
-    <li className="px-4 py-3 border-b border-solid border-slate-700 bg-rich-black/75 backdrop-blur-lg backdrop-saturate-150 flex flex-col lg:flex-row lg:justify-between gap-5">
+    <li className="px-4 py-3 border-b border-solid border-slate-700 bg-rich-black/75 backdrop-blur-sm backdrop-saturate-150 flex flex-col lg:flex-row lg:justify-between gap-5">
       <CommitInfo commit={commit} />
       <CommitButtons commit={commit} />
     </li>
