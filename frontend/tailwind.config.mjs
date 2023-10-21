d7c8be8f785d190fedcacbfccb9b952cc0377f1b
jorgeabrahan <jorge24abrahan@gmail.com>
2023-10-21 06:20:27 -0600

    Update: add extended color to tailwind configuration

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'rich-black': {
+					DEFAULT: '#111928'
+				}
+			}
+		},
 	},
 	plugins: [],
 }
