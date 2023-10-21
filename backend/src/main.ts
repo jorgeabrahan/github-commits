e3f7930941e8530691fb8a18828f4e2f828b9d38
jorgeabrahan <jorge24abrahan@gmail.com>
2023-10-20 18:20:16 -0600

    Update: nestjs configuration file to allow requests from frontend URL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // permitir las peticiones desde el frontend
+  app.enableCors({
+    origin: 'http://localhost:4321',
+  });
   await app.listen(3000);
 }
 bootstrap();
