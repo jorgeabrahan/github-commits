b47001c593b48d42b105c94fd3c93d810505fd4c
jorgeabrahan <jorge24abrahan@gmail.com>
2023-10-20 18:43:16 -0600

    Update: methods to use try/catch and throw significant errors from requests

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -12,32 +12,48 @@ export class AppService {
   }
 
   async getCommits() {
-    const { data } = await axios.get(`${this.baseUrl}/commits`);
-    return data;
+    try {
+      const { data } = await axios.get(`${this.baseUrl}/commits`);
+      return data;
+    } catch (_) {
+      throw new Error('Error while fetching commits from GitHub');
+    }
   }
 
   async getCommitsByAuthor(author: string) {
-    const { data } = await axios.get(
-      `${this.baseUrl}/commits?author=${author}`,
-    );
-    return data;
+    try {
+      const { data } = await axios.get(
+        `${this.baseUrl}/commits?author=${author}`,
+      );
+      return data;
+    } catch (_) {
+      throw new Error('Error while fetching commits by user from GitHub');
+    }
   }
 
   async getCommitsByDate(dateSince: string, dateUntil: string) {
-    const { data } = await axios.get(
-      `${this.baseUrl}/commits?since=${dateSince}T00:00:00Z&until=${dateUntil}T23:59:59Z`,
-    );
-    return data;
+    try {
+      const { data } = await axios.get(
+        `${this.baseUrl}/commits?since=${dateSince}T00:00:00Z&until=${dateUntil}T23:59:59Z`,
+      );
+      return data;
+    } catch (_) {
+      throw new Error('Error while fetching commits by date from GitHub');
+    }
   }
 
   async getCommitsByKeyword(keyword: string) {
-    const { data } = await axios.get(`${this.baseUrl}/commits`);
-    return data.filter(
-      (commit) =>
-        commit?.commit?.message
-          ?.toLowerCase()
-          ?.includes(keyword?.toLowerCase()),
-    );
+    try {
+      const { data } = await axios.get(`${this.baseUrl}/commits`);
+      return data.filter(
+        (commit) =>
+          commit?.commit?.message
+            ?.toLowerCase()
+            ?.includes(keyword?.toLowerCase()),
+      );
+    } catch (_) {
+      throw new Error('Error while fetching commits by keyword from GitHub');
+    }
   }
 
   async getCommitsByFilters(
@@ -61,19 +77,23 @@ export class AppService {
     }
 
     if (params.length > 0) url += '?' + params.join('&');
-    const { data } = await axios.get(url);
+    try {
+      const { data } = await axios.get(url);
 
-    let commits = data;
-    // if a keyword is present
-    if (keyword) {
-      // filter comments by keyword
-      commits = commits.filter(
-        (commit) =>
-          commit?.commit?.message
-            ?.toLowerCase()
-            ?.includes(keyword?.toLowerCase()),
-      );
+      let commits = data;
+      // if a keyword is present
+      if (keyword) {
+        // filter comments by keyword
+        commits = commits.filter(
+          (commit) =>
+            commit?.commit?.message
+              ?.toLowerCase()
+              ?.includes(keyword?.toLowerCase()),
+        );
+      }
+      return commits;
+    } catch (_) {
+      throw new Error('Error while fetching commits by filters from GitHub');
     }
-    return commits;
   }
 }
