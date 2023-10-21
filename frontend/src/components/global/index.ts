8a23e136402e8104c40ca09403d2aa6ce74e9e26
jorgeabrahan <jorge24abrahan@gmail.com>
2023-10-21 15:58:02 -0600

    Create: index file for imports from the global folder

---
+++ frontend/src/components/global/index.ts
@@ -0,0 +1,4 @@
+export { LoadingIcon } from './LoadingIcon'
+export { NothingFound } from './NothingFound'
+export { InputFilter } from './InputFilter'
+export { CommitsAndFilters } from './CommitsAndFilters'
