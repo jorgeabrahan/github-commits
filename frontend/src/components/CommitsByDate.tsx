ab67a7f116ab8cafce72cd036045d4be47866a7a
jorgeabrahan <jorge24abrahan@gmail.com>
2023-10-21 22:37:03 -0600

    Update: import external helper to determine if dates are valid

--- frontend/src/components/CommitsByDate.tsx
+++ frontend/src/components/CommitsByDate.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { API_BASE_URL } from '../config/apiConfig'
-import { showToastError, showToastSuccess } from '../helpers'
+import { areDatesValid, showToastError, showToastSuccess } from '../helpers'
 import { CommitsAndFilters } from './global'
 import { useDebounce } from '@uidotdev/usehooks'
 import { InputDateFilter } from './global/InputDateFilter'
@@ -48,52 +48,4 @@ export const CommitsByDate = () => {
       </form>
     </CommitsAndFilters>
   )
-}
-
-const validateDate = (date: string): boolean => {
-  const [year, month, day] = date.split('-')
-  if (isNaN(Number(year)) || isNaN(Number(month)) || isNaN(Number(day))) {
-    showToastError(`Date ${date} is not valid`)
-    return false
-  }
-  if (year.length > 4 || year.length < 1) {
-    showToastError(`Year ${year} is not valid!`)
-    return false
-  }
-  if (Number(year) < 2023) {
-    showToastError(`Year ${year} should be greater than 2023`)
-    return false
-  }
-  if (month.length > 2 || month.length < 1) {
-    showToastError(`Month ${month} is not valid!`)
-    return false
-  }
-  if (Number(year) === 2023 && Number(month) < 10) {
-    showToastError(`Month ${month} should be greater than 10 if year is ${year}`)
-    return false
-  }
-  if (day.length > 2 || day.length < 1) {
-    showToastError(`Day ${day} is not valid!`)
-    return false
-  }
-  
-  return true
-}
-
-const areDatesValid = (dates: { dateSince: string; dateUntil: string }): boolean => {
-  // for each date
-  for (const date of Object.values(dates)) {
-    if (!validateDate(date)) {
-      return false;
-    }
-  }
-
-  const datesOffset = new Date(dates?.dateUntil).getTime() - new Date(dates?.dateSince).getTime()
-  
-  if (datesOffset < 0) {
-    showToastError('Date until should be greater or equal to date since')
-    return false
-  }
-
-  return true
 }
