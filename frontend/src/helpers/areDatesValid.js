bc9a417e8ef1a0d65433c22c9cb1448013f0b0c5
jorgeabrahan <jorge24abrahan@gmail.com>
2023-10-21 22:36:38 -0600

    Create: helper to determine if dates are valid

---
+++ frontend/src/helpers/areDatesValid.js
@@ -0,0 +1,44 @@
+const validateDate = (date) => {
+  const [year, month, day] = date.split('-')
+  if (isNaN(Number(year)) || isNaN(Number(month)) || isNaN(Number(day))) {
+    showToastError(`Date ${date} is not valid`)
+    return false
+  }
+  if (year.length > 4 || year.length < 1) {
+    showToastError(`Year ${year} is not valid!`)
+    return false
+  }
+  if (Number(year) < 2023) {
+    showToastError(`Year ${year} should be greater than 2023`)
+    return false
+  }
+  if (month.length > 2 || month.length < 1) {
+    showToastError(`Month ${month} is not valid!`)
+    return false
+  }
+  if (Number(year) === 2023 && Number(month) < 10) {
+    showToastError(`Month ${month} should be greater than 10 if year is ${year}`)
+    return false
+  }
+  if (day.length > 2 || day.length < 1) {
+    showToastError(`Day ${day} is not valid!`)
+    return false
+  }
+  
+  return true
+}
+
+export const areDatesValid = (dates) => {
+  // for each date
+  for (const date of Object.values(dates)) {
+    if (!validateDate(date)) {
+      return false;
+    }
+  }
+  const datesOffset = new Date(dates?.dateUntil).getTime() - new Date(dates?.dateSince).getTime()
+  if (datesOffset < 0) {
+    showToastError('Date until should be greater or equal to date since')
+    return false
+  }
+  return true
+}
--- frontend/src/helpers/index.js
+++ frontend/src/helpers/index.js
@@ -1,9 +1,11 @@
 import { showToastSuccess, showToastError } from "./showToast";
 import { calcTimeOffset } from "./calcTimeOffset";
 import { monthNumberToString } from "./monthNumberToString";
+import { areDatesValid } from "./areDatesValid";
 export {
   showToastSuccess,
   showToastError,
   calcTimeOffset,
   monthNumberToString,
+  areDatesValid
 }
